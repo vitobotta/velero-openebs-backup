a47281ccbc5208a1d500b9e8e86c3fa9c93e3320
Vito Botta <vito@botta.me>
2019-06-12 15:12:53 +0300

    Take last backup, not the first, to get the name of the backup newly started when creating a schedule

--- lib/backup.rb
+++ lib/backup.rb
@@ -21,7 +21,7 @@ class Backup < Struct.new(:options)
     `#{backup_command}`
 
     if options[:schedule]
-      backup_name = `kubectl -n velero get backup --selector 'velero.io/schedule-name=#{options[:backup]}' --sort-by=.status.startTimestamp -o jsonpath='{.items[*].metadata.name}'`.split.first
+      backup_name = `kubectl -n velero get backup --selector 'velero.io/schedule-name=#{options[:backup]}' --sort-by=.status.startTimestamp -o jsonpath='{.items[*].metadata.name}'`.split.last
       sleep 2 # to give time to start the backup
       puts "Backup scheduled and started. Run `velero describe backup #{backup_name} --details` for progress about the current backup."
     else
